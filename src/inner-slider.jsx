0f30fdfda124cf0437c4cc60341c5db13ecfb769
Renato Duarte <renato.duarte@beubi.com>
2016-07-28 10:56:52 +0100

    react-slick add custom track left

--- src/inner-slider.jsx
+++ src/inner-slider.jsx
@@ -89,6 +89,7 @@ export var InnerSlider = React.createClass({
       lazyLoad: this.props.lazyLoad,
       lazyLoadedList: this.state.lazyLoadedList,
       rtl: this.props.rtl,
+      customTrackLeft: this.props.customTrackLeft,
       slideWidth: this.state.slideWidth,
       slidesToShow: this.props.slidesToShow,
       slideCount: this.state.slideCount,
